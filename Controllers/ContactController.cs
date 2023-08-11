36c25965be68bf96fb7e7ef3fdfc785d0d5a3bd9
Giovanni Neves Sadauscas <giovanni_neves@tutanota.com>
2023-08-11 15:17:25 -0300

    Conexão com o banco Resposta em TempData Salvar

--- Controllers/ContactController.cs
+++ Controllers/ContactController.cs
@@ -1,3 +1,4 @@
+using cs_mpp.Data;
 using cs_mpp.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -5,14 +6,42 @@ namespace cs_mpp.Controllers;
 
 public class ContactController : Controller
 {
+
+    private readonly ApplicationDbContext _context;
+
+    public ContactController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
     public IActionResult Index()
     {
         return View();
     }
 
     [HttpPost]
-    public IActionResult Register(Contact contact)
+    public IActionResult Index(Contact contact)
     {
-        return Ok(contact);
+        if (!ModelState.IsValid)
+        {
+            return View(contact);
+        }
+
+        DateTime dateTime = DateTime.Now;
+        contact.Create_at = dateTime;
+
+        try
+        {
+            _context.Contacts.Add(contact);
+            _context.SaveChanges();
+            TempData["SuccessMessage"] = "Os dados de contato foram enviados com sucesso!";
+        }
+        catch (Exception)
+        {
+            TempData["FailMessage"] = "Ocorreu um erro ao enviar, tente novamente mais tarde.";
+        }
+
+        return RedirectToAction("Index");
     }
 }
