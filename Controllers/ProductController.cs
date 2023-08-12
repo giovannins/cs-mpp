58ac7de9ddfee43ed7eb9af209e7ede2089e2fbe
Giovanni <giovanni_neves@tutanota.com>
2023-08-12 16:27:02 -0300

    Adicionando View e chamada no DB Adicionando Index e chamada no DB

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,11 +1,43 @@
+using cs_mpp.Data;
+using cs_mpp.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace cs_mpp.Controllers;
 
 public class ProductController : Controller
 {
-    public IActionResult Index()
+
+    private readonly ApplicationDbContext _context;
+
+    public ProductController(ApplicationDbContext context)
     {
-        return View();
+        _context = context;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        List<Product> products = await _context.Products.ToListAsync();
+        products.ForEach(product => product.Image = GetOnlyTheFirstImage(product.Image));
+        return View(products);
+    }
+
+    public async Task<IActionResult> View(long id)
+    {
+        Product product = await _context.Products.FirstOrDefaultAsync(predicate: predicate => predicate.Id == id);
+        return View(product);
+    }
+
+
+    // Funções de produtos
+
+    private static string GetOnlyTheFirstImage(string Images)
+    {
+        string[] urls = Images.Split('|');
+        if (urls.Length == 0)
+        {
+            return "";
+        }
+        return urls[0];
     }
 }
